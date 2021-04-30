caa6bdd1eaf41b9fb281a4d541ee0026734a8b95
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2021-05-01 00:51:46 +0100

    Release Version 1.0.8.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xkcd-pass",
-    version="1.0.7",
+    version="1.0.8",
     author="Adam Birds",
     author_email="adam.birds@adbwebdesigns.co.uk",
     description="Generate secure multiword passwords/passphrases, inspired by XKCD",
