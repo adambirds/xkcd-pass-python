51b3bc9ecb2a4b24c47057c55835c5b4dad5fd9f
pauljosephanderson <p.anderson@iinet.net.au>
2022-12-17 14:13:06 +1100

    Create xkcd_default.py
    Centralising default values

---
+++ src/xkcd_pass/lib/xkcd_default.py
@@ -0,0 +1,15 @@
+"""Default Settings file for xkcd_pass modules."""
+
+DEFAULT_WORDFILE = "eff-long"
+MIN_LENGTH = 5
+MAX_LENGTH = 9
+NUM_WORDS = 4
+NO_PADDING_DIGITS = False
+PADDING_DIGITS_NUM = 2
+INTERACTIVE = False
+VALID_CHARS  = ["a-z"]
+VERBOSE = False
+COUNT = 1
+DELIM = ""
+SEP = "\n"
+CASE = "first"
